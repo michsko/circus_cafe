87afc424fd49a6a054f392fca0c5b9b15cce7dad
michsko <michal.skocovsky@protonmail.com>
2022-12-28 09:44:44 +0100

    search and profile forms

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blog.html")
+
+
+@app.route("/forum")
+def forum():
+    return render_template("forum.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("hledat.html")
+
+@app.route("/news")
+def news():
+    return render_template("zpravy.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+
+
 
 
 
